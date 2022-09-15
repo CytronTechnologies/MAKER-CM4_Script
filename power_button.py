c3d94f3090d89e41fae5ec8b7d3bded77698bb2c
Kong Wai Weng <waiweng83@gmail.com>
2022-09-15 11:59:45 +0800

    Added script for safe shutdown.

---
+++ power_button.py
@@ -0,0 +1,14 @@
+import os
+import RPi.GPIO as GPIO
+
+# Initialize pin.
+POWER_BUTTON = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(POWER_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Wait for button press and shutdown the CM4 safely.
+white True:
+    GPIO.wait_for_edge(POWER_BUTTON, GPIO.FALLING)
+    os.system("sudo shutdown -h now")
