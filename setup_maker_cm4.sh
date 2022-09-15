70e32900bf7820c7b2d2a151331662aa2ab82b0a
Kong Wai Weng <waiweng83@gmail.com>
2022-09-15 15:39:26 +0800

    Configure the config.txt file too.

--- setup_maker_cm4.sh
+++ setup_maker_cm4.sh
@@ -2,11 +2,13 @@
 
 # Make sure the script is run as root.
 if [ $(id -u) -ne 0 ]; then
+    echo
+    echo
     echo "Please run as root."
     exit 1
 fi
 
-echo "Installing power button script..."
+
 
 # Download script.
 cd /usr/local/bin
@@ -15,12 +17,45 @@ cd maker_cm4
 curl -LO https://raw.githubusercontent.com/CytronTechnologies/MAKER-CM4_Script/main/power_button.py
 
 # Add the command to run the script to rc.local if it's not there yet.
-grep maker_cm4 /etc/rc.local >/dev/null
+rc_file = "/etc/rc.local"
+grep "maker_cm4" $rc_file >/dev/null
 if [ $? -ne 0 ]; then
-    # Insert gpio-halt into rc.local before final 'exit 0'
-    sed -i "s/^exit 0/sudo python \/usr\/local\/bin\/maker_cm4\/power_button.py \& \nexit 0/g" /etc/rc.local
+    # Insert into rc.local before final 'exit 0'
+    sed -i "s/^exit 0/sudo python \/usr\/local\/bin\/maker_cm4\/power_button.py \&\nexit 0/g" $rc_file
 fi
 
+
+
+# Configure the config.txt file.
+config_file = "/boot/config.txt"
+
+# Disable USB OTG.
+grep "#otg_mode=1" $config_file >/dev/null
+if [ $? -ne 0 ]; then
+    sed -i "s/^otg_mode=1/#otg_mode=1/g" $config_file
+fi
+
+# Enable USB host.
+grep "dtoverlay=dwc2,dr_mode=host" $config_file >/dev/null
+if [ $? -ne 0 ]; then
+    echo "dtoverlay=dwc2,dr_mode=host" >> $config_file
+fi
+
+# Enable I2C for RTC.
+grep "dtparam=i2c_vc=on" $config_file >/dev/null
+if [ $? -ne 0 ]; then
+    echo "dtparam=i2c_vc=on" >> $config_file
+    echo "dtoverlay=i2c-rtc,pcf85063a,i2c_csi_dsi" >> $config_file
+fi
+
+# Remap audio.
+grep "dtoverlay=audremap,pins_18_19" $config_file >/dev/null
+if [ $? -ne 0 ]; then
+    echo "dtoverlay=audremap,pins_18_19" >> $config_file
+fi
+
+
+echo
 echo
 echo "######################################"
 echo "Cytron CM4 Maker Board Setup Completed"
